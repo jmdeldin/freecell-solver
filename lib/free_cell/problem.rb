df9c59c658cb88c5b7aa5ced9af8924e2f880d71
Jon-Michael Deldin <dev@jmdeldin.com>
2012-09-30 14:15:14 -0600

    Nest Problem in a module to ease using other classes

--- lib/free_cell/problem.rb
+++ lib/free_cell/problem.rb
@@ -1,54 +1,67 @@
-class FreeCell::Problem
-  def initialize(opts)
-    @columns = opts.fetch :columns
+module FreeCell
+  class Problem
+    def initialize(opts)
+      @columns = opts.fetch :columns
 
-    @num_foundations = opts[:num_foundations] || 2
-    @foundations = {:spades => [], :hearts => []}
-    if @num_foundations == 4
-      @foundations.merge!({:diamonds => [], :clubs => []})
-    end
-  end
+      @num_foundations = opts[:num_foundations] || 2
+      @foundations = {:spades => [], :hearts => []}
+      if @num_foundations == 4
+        @foundations.merge!({:diamonds => [], :clubs => []})
+      end
 
-  # See if our goal state is true, which is when all of the foundations are
-  # filled. To avoid iterating and comparing each foundation, we assume that
-  # empty columns and empty free cells means all of the foundations are
-  # filled. This assumes there was no wind on the table making cards
-  # disappear.
-  #
-  # TODO: This might need to be optimized to avoid potentially expensive array
-  # length calls.
-  def solved?
-    empty_columns? && empty_free_cells?
-  end
-
-  def empty_columns?
-    @columns.flatten.empty?
-  end
-
-  def empty_free_cells?
-    @free_cells.empty?
-  end
-
-  def empty_foundations?
-    @foundations.flatten.empty?
-  end
-
-  def can_insert_into_foundation?(card)
-    f = @foundations[card.suit]
-
-    card.face == 'A' || card.sequentially_larger_than?(f.last)
-  end
-
-
-  # Returns the available actions based on the current columns, free cells,
-  # and foundations.
-  def actions
-    c = current_card
-    actions = []
-
-    if can_insert_into_foundation? c
-      actions << :move_to_foundation
+      @free_cells = Array.new(opts[:num_free_cells] || 4)
     end
 
+    # See if our goal state is true, which is when all of the foundations are
+    # filled. To avoid iterating and comparing each foundation, we assume that
+    # empty columns and empty free cells means all of the foundations are
+    # filled. This assumes there was no wind on the table making cards
+    # disappear.
+    #
+    # TODO: This might need to be optimized to avoid potentially expensive array
+    # length calls.
+    def solved?
+      empty_columns? && empty_free_cells?
+    end
+
+    def empty_columns?
+      @columns.flatten.empty?
+    end
+
+    def empty_free_cells?
+      @free_cells.empty?
+    end
+
+    def empty_foundations?
+      @foundations.flatten.empty?
+    end
+
+    def can_insert_into_foundation?(card)
+      f = @foundations[card.suit]
+
+      card.face == 'A' || card.sequentially_larger_than?(f.last)
+    end
+
+
+    # Returns the available actions based on the current columns, free cells,
+    # and foundations.
+    def actions
+      actions = []
+
+      # free cell to foundation
+      @free_cells.each do |free_cell|
+        m = FreeToFoundationMove.new(self, free_cell)
+        # actions << m if m.valid?
+      end
+
+      # last exposed card to foundation
+      # column to column
+      # column to free
+
+      # if can_insert_into_foundation? c
+      #   actions << :move_to_foundation
+      # end
+
+    end
   end
 end
